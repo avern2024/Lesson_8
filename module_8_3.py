561424ded3b8b5a85cc46fab8781eac592b05876
avern2024 <avern2020@icloud.com>
2024-11-19 22:25:48 +0700

    Add files via upload

--- module_8_3.py
+++ module_8_3.py
@@ -13,21 +13,19 @@ class Car:
         self.model = model
         self.__vin = vin
         self.__numbers = numbers
-        self.__is_valid_vin()
-        self.__is_valid_numbers()
+        self.__is_valid_vin(self.__vin)
+        self.__is_valid_numbers(self.__numbers)
 
-    def __is_valid_vin(self):
-        vin_number = self.__vin
+    def __is_valid_vin(self, vin_number):
         if not isinstance(vin_number, int):
             raise IncorrectVinNumber('Некорректный тип vin номер')
         if not 1000000 <= vin_number <= 9999999:
             raise IncorrectVinNumber('Неверный диапазон для vin номера')
         return True
 
-    def __is_valid_numbers(self):
-        numbers = self.__numbers
+    def __is_valid_numbers(self, numbers):
         if not isinstance(numbers, str):
-            raise IncorrectCarNumbers('Неверный диапазон для vin номера')
+            raise IncorrectCarNumbers('Некорректный тип данных для номеров')
         if len(numbers) != 6:
             raise IncorrectCarNumbers('Неверная длина номера')
         return True
