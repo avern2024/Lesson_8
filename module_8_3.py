c5efda1042df1eff8babe7cf42deabebd6c2d7b7
avern2024 <avern2020@icloud.com>
2024-11-20 08:17:45 +0700

    Add files via upload

--- module_8_3.py
+++ module_8_3.py
@@ -9,53 +9,58 @@ class IncorrectCarNumbers(Exception):
 
 
 class Car:
-    def __init__(self, model: str, vin: int, numbers: str):
+    def __init__(self, model, vin, numbers):
         self.model = model
-        self.__vin = vin
-        self.__numbers = numbers
-        self.__is_valid_vin(self.__vin)
-        self.__is_valid_numbers(self.__numbers)
+        self.__vin = self.__set_vin(vin)  # Устанавливаем vin через метод
+        self.__numbers = self.__set_numbers(numbers)  # Устанавливаем numbers через метод
 
-    def __is_valid_vin(self, vin_number):
+    def __is_valid_vin(self, vin_number):  # Метод проверки VIN
         if not isinstance(vin_number, int):
             raise IncorrectVinNumber('Некорректный тип vin номер')
         if not 1000000 <= vin_number <= 9999999:
             raise IncorrectVinNumber('Неверный диапазон для vin номера')
         return True
 
-    def __is_valid_numbers(self, numbers):
+    def __is_valid_numbers(self, numbers):  # Метод проверки номера
         if not isinstance(numbers, str):
             raise IncorrectCarNumbers('Некорректный тип данных для номеров')
         if len(numbers) != 6:
             raise IncorrectCarNumbers('Неверная длина номера')
         return True
 
+    def __set_vin(self, vin):  # Метод для установки VIN
+        self.__is_valid_vin(vin)
+        return vin  # Возвращаем валидное значение
 
-if __name__ == '__main__':
-    # Пример выполнения кода
-    try:
-        first = Car('Model1', 1000000, 'f123dj')
-    except IncorrectVinNumber as exc:
-        print(exc.message)
-    except IncorrectCarNumbers as exc:
-        print(exc.message)
-    else:
-        print(f'{first.model} успешно создан')
+    def __set_numbers(self, numbers):  # Метод для установки номера
+        self.__is_valid_numbers(numbers)
+        return numbers  # Возвращаем валидное значение
 
-    try:
-        second = Car('Model2', 300, 'т001тр')
-    except IncorrectVinNumber as exc:
-        print(exc.message)
-    except IncorrectCarNumbers as exc:
-        print(exc.message)
-    else:
-        print(f'{second.model} успешно создан')
 
-    try:
-        third = Car('Model3', 2020202, 'нет номера')
-    except IncorrectVinNumber as exc:
-        print(exc.message)
-    except IncorrectCarNumbers as exc:
-        print(exc.message)
-    else:
-        print(f'{third.model} успешно создан')
+# Пример выполнения кода
+try:
+    first = Car('Model1', 1000000, 'f123dj')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{first.model} успешно создан')
+
+try:
+    second = Car('Model2', 300, 'т001тр')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{second.model} успешно создан')
+
+try:
+    third = Car('Model3', 2020202, 'нет номера')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{third.model} успешно создан')
